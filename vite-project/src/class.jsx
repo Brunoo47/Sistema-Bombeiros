eb9273c7e241a54ccdf3058aa2614db8ca059680
Lucas Klug Sebastião <lucasklugs@gmail.com>
2023-11-27 21:53:50 -0300

    Class Teste

---
+++ vite-project/src/class.jsx
@@ -0,0 +1,319 @@
+class MetodoHistorico1 extends Component {
+    state = {
+        Aspiracao: "",
+        Rolamento180: "",
+        AvaliacaoInicial: "",
+        TomadaDecisao: "",
+        AvaliacaoDirigida: "",
+        TratadoChoque: "",
+        AvaliacaoContinuada: "",
+        UsoCanula: "",
+        ChaveRautek: "",
+        UsoColar: "",
+        LocalColar: "",
+        CanulaGuedel: "",
+        UsoKED: "",
+        DesobstrucaoVA: "",
+        UsoTTF: "",
+        EmpregoDEA: "",
+        VentilacaoSuporte: "",
+        GerenciamentoRiscos: "",
+        Oxigenioterapia: "",
+        LimpezaCurativos: "",
+        Reanimador1: "",
+        Curativos1: "",
+        Reanimador2: "",
+        Compressivo: "",
+        MeiosAuxiliares: "",
+        LocalMeiosAuxiliares: "",
+        Encravamento: "",
+        Ocular: "",
+        Queimadura: "",
+        Simples: "",
+        TresPontas: "",
+        Imobilizacoes: "",
+        LocalImobilizacoes: "",
+        MacaSobreRodas: "",
+        MacaRigida: "",
+        Ponte: "",
+        RetiradoCapacete: "",
+        RCP: "",
+        Rolamento90: "",
+        OutraOcorrencia: "",
+        TipoOcorrencia: ""
+    };
+
+}
+
+class MetodoHistorico2 extends Component {
+    state = {
+        Ataduras: "",
+        CateterTPOculos: "",
+        Kits: "",
+        LuvasDescPares: "",
+        MascaraDesc: "",
+        MantaAluminizada: "",
+        PasDEA: "",
+        SondaAspiracao: "",
+        SoroFisiologico: "",
+        TalasPAP: "",
+        OutroMaterial1: "",
+        OutroMaterial2: "",
+        OutroMaterial3: "",
+        OutroMaterial4: "",
+        OutroMaterial5: "",
+        OutroMaterial6: "",
+    };
+
+}
+
+class MetodoHistorico3 extends Component {
+    state = {
+        sinaisESintomas: "",
+        aconteceuOutrasVezes: "",
+        tempoOcorrencia: "",
+        problemaSaude: "",
+        alergico: "",
+        ingeriuAlimento: "",
+    };
+
+}
+
+class MetodoHistorico4 extends Component {
+    state = {
+        periodoGestacao: "",
+        fezPreNatal: "",
+        nomeMedico: "",
+        complicacoes: "",
+        primeiroFilho: "",
+        quantidadeFilhos: "",
+        inicioContracoes: "",
+        duracaoIntervalo: "",
+    };
+
+}
+
+class MetodoHistorico5 extends Component {
+    state = {
+        disturbioComportamento: "",
+        capaceteEncontrado: "",
+        cintoEncontrado: "",
+        paraBrisasAvariado: "",
+        caminhandoCena: "",
+        painelAvariado: "",
+        volanteTorcido: "",
+
+    };
+
+}
+
+class MetodoOcorrencia extends Component {
+    state = {
+        CausadoPorAnimais: '',
+        DesmoronamentoDeslizamento: '',
+        Afogamento: '',
+        QuedaDeAltura: '',
+        Atropelamento: '',
+        Desabamento: '',
+        Esportivo: '',
+        QuedaDeBicicleta: '',
+        ComMeioDeTransporte: '',
+        EmergenciaMedica: '',
+        TentativaSuicidio: '',
+        Agressao: '',
+        ChoqueEletrico: '',
+        Domestico: '',
+        Intoxicacao: '',
+        QuedaDeMoto: '',
+        Transferencia: '',
+    };
+
+}
+
+class MetodoOcorrencia2 extends Component {
+    state = {
+        psiquiatrico: "",
+        respiratorio: "",
+        diabetes: "",
+        obstetrico: "",
+    };
+
+}
+
+class MetodoOcorrencia3 extends Component {
+    state = {
+        AberturaOcular: "",
+        RespostaVerbal: "",
+        RespostaMotora: "",
+    };
+
+}
+
+
+class MetodoOcorrencia4 extends Component {
+    state = {
+        ha_outra_ocorrencia: "",
+        AbdomenSensivelRigido: "",
+        EstaseJugular: "",
+        AfundamentoCranio: "",
+        FacePalida: "",
+        Agitacao: "",
+        Hemorragia: "",
+        HemorragiaTipo: "",
+        Amnesia: "",
+        Hipertensao: "",
+        Apneia: "",
+        Hiponeia: "",
+        AnginaPeito: "",
+        NauseasVomitos: "",
+        Bradicardia: "",
+        Nasorragia: "",
+        Bradipneia: "",
+        Obito: "",
+        BroncoAspirando: "",
+        Otorreia: "",
+        Cefaleia: "",
+        Otorragia: "",
+        Cianose: "",
+        CianoseLocal: "",
+        O_V_A_C_E: "",
+        Convulsao: "",
+        Parada: "",
+        ParadaTipo: "",
+        DesvioTraqueia: "",
+        Priaprismo: "",
+        DorLocal: "",
+        PruridoPele: "",
+        Edema: "",
+        EdemaTipo: "",
+        Pupilas: "",
+        PupilasTipo: "",
+        EnfisemaSubcutaneo: "",
+        Sede: "",
+        Taquipneia: "",
+        SinalBattle: "",
+        Taquicardia: "",
+        SinalGuaxinim: "",
+        Tontura: "",
+        Sudorese: "",
+        OutraOcorrencia: "",
+    };
+
+}
+
+
+class MetodoOcorrencia4 extends Component {
+    state = {
+        ha_outra_ocorrencia: "",
+        AbdomenSensivelRigido: "",
+        EstaseJugular: "",
+        AfundamentoCranio: "",
+        FacePalida: "",
+        Agitacao: "",
+        Hemorragia: "",
+        HemorragiaTipo: "",
+        Amnesia: "",
+        Hipertensao: "",
+        Apneia: "",
+        Hiponeia: "",
+        AnginaPeito: "",
+        NauseasVomitos: "",
+        Bradicardia: "",
+        Nasorragia: "",
+        Bradipneia: "",
+        Obito: "",
+        BroncoAspirando: "",
+        Otorreia: "",
+        Cefaleia: "",
+        Otorragia: "",
+        Cianose: "",
+        CianoseLocal: "",
+        O_V_A_C_E: "",
+        Convulsao: "",
+        Parada: "",
+        ParadaTipo: "",
+        DesvioTraqueia: "",
+        Priaprismo: "",
+        DorLocal: "",
+        PruridoPele: "",
+        Edema: "",
+        EdemaTipo: "",
+        Pupilas: "",
+        PupilasTipo: "",
+        EnfisemaSubcutaneo: "",
+        Sede: "",
+        Taquipneia: "",
+        SinalBattle: "",
+        Taquicardia: "",
+        SinalGuaxinim: "",
+        Tontura: "",
+        Sudorese: "",
+        OutraOcorrencia: "",
+    };
+
+}
+
+
+class MetodoOcorrencia5 extends Component {
+    state = {
+        pressao_arterial: "",
+        pulso: "",
+        respiracao: "",
+        saturacao: "",
+        hgt: "",
+        temperatura: "",
+    };
+
+}
+
+class MetodoOcorrencia6 extends Component {
+    state = {
+        local: "",
+        lado: "",
+        tipo: "",
+        face: "",
+    };
+
+}
+
+class MetodoOcorrencia7 extends Component {
+    state = {
+        tipo_queimadura: "",
+        local: "",
+        grau: "",
+    };
+
+}
+
+class MetodoOcorrencia8 extends Component {
+    state = {
+        Sentada: "",
+        Deitada: "",
+        SemiDeitada: "",
+        Ciclista: "",
+        Trauma: "",
+        Condutor_moto: "",
+        Pass_Banco_de_Tras: "",
+        Gestante: "",
+        Pedestre: "",
+        Pass_Banco_da_Frente: "",
+        Pass_Moto: "",
+        Condutor_carro: "",
+        Clínico: "",
+
+    };
+
+}
+
+class MetodoOcorrencia8 extends Component {
+    state = {
+        Critico: false,
+        Instavel: false,
+        Potencialmente_instavel: false,
+        Estavel: false,
+    };
+
+}
+
+
+
